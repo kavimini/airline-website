e01278bb23e4340c524323f38a4f068ed4352f93
kavisha <kavishak07@gmail.com>
2022-11-14 11:31:39 +0800

    add popup to navbar functionality

--- airline-system/js/script.js
+++ airline-system/js/script.js
@@ -59,8 +59,37 @@ var responsiveSlider = function() {
       }, 7000);
       
       };
+
+var navpopup = function(id,value) {
+    var popup = document.getElementById(id);
+
+      // Get the button that opens the popup
+    var btn = document.getElementById("button-"+value);
+    
+    var index = parseInt(value) - 1;
+    var span = document.getElementsByClassName("close")[index];
+    
+          // When the user clicks on the button, open the popup
+    btn.onclick = function() {
+        popup.style.display = "block";
+    }
+          
+          // When the user clicks on <span> (x), close the popup
+      span.onclick = function() {
+        popup.style.display = "none";
+      }
+            
+          // When the user clicks anywhere outside of the popup, close it
+    window.onclick = function(event) {
+      if (event.target == popup) {
+            popup.style.display = "none";
+          }
+      }
+
+}
       
       window.onload = function() {
-      responsiveSlider();  
-      }
+      responsiveSlider(); 
       
+      }
+    
